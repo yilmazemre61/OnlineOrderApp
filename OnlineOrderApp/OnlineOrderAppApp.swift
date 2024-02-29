5a2f1ab661602572aaa5b9b5189fd7a9b7de61fe
yilmazemre61 <emreyilmz1@gmail.com>
2024-02-28 20:37:48 -0500

    Initial Commit

---
+++ OnlineOrderApp/OnlineOrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineOrderAppApp.swift
+//  OnlineOrderApp
+//
+//  Created by Emre Yilmaz on 2024-02-28.
+//
+
+import SwiftUI
+
+@main
+struct OnlineOrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
